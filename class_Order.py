c9a67a419e040bae8ab4cf89f89ed14f22724bed
vladiemir1 <114985274+vladiemir1@users.noreply.github.com>
2022-11-28 21:12:19 +0300

    Update class_Order.py

--- class_Order.py
+++ class_Order.py
@@ -1,4 +1,8 @@
 import math
 from sympy import Point
 class Orders:
-    def __init__(self,coordinate,
+    def __init__(self,number, x, y, weight):
+        self.number = number
+        self.x = x
+        self.y = y
+        self.weight = weight
